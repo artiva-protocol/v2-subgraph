210c810ad8183f823f9eafa7eb47b0022ce84165
neokry <patrickgenevich@gmail.com>
2022-08-01 09:50:11 -0700

    inital commit

--- src/observability.ts
+++ src/observability.ts
@@ -1,67 +1,81 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  Observability,
   CloneDeployed,
   ContentDigestAdded,
   ContentDigestRemoved,
   FactoryImplementationSet,
   PlatformMetadataDigestSet,
-  RoleSet
-} from "../generated/Observability/Observability"
-import { ExampleEntity } from "../generated/schema"
+  RoleSet,
+} from "../generated/Observability/Observability";
+import { Platform, ContentDigest, PlatformUser } from "../generated/schema";
 
 export function handleCloneDeployed(event: CloneDeployed): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let platform = new Platform(event.params.clone.toHex());
+  platform.metadataDigestUpdatedTimestamp = event.block.timestamp.toString();
+  platform.deployedAtTimestamp = event.block.timestamp.toString();
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.factory = event.params.factory
-  entity.owner = event.params.owner
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // None
+  platform.save();
 }
 
-export function handleContentDigestAdded(event: ContentDigestAdded): void {}
+export function handleContentDigestAdded(event: ContentDigestAdded): void {
+  let contentDigest = new ContentDigest(event.params.digest.toBase58());
 
-export function handleContentDigestRemoved(event: ContentDigestRemoved): void {}
+  contentDigest.platform = event.params.clone.toHex();
+  contentDigest.owner = event.params.owner.toHex();
+  contentDigest.addedAtTimestamp = event.block.timestamp.toString();
+  contentDigest.removedAtTimestamp = null;
+  contentDigest.status = "CURATED";
+  contentDigest.save();
+
+  let platform = new Platform(event.params.clone.toHex());
+  platform.contentAddedTimestamp = event.block.timestamp.toString();
+  platform.save();
+}
+
+export function handleContentDigestRemoved(event: ContentDigestRemoved): void {
+  let contentDigest = new ContentDigest(event.params.digest.toBase58());
+  contentDigest.removedAtTimestamp = event.block.timestamp.toString();
+  contentDigest.status = "REMOVED";
+  contentDigest.save();
+}
+
+export function handlePlatformMetadataDigestSet(
+  event: PlatformMetadataDigestSet
+): void {
+  let platform = new Platform(event.params.clone.toHex());
+  platform.platformMetadataDigest = event.params.platformMetadataDigest;
+  platform.metadataDigestUpdatedTimestamp = event.block.timestamp.toString();
+  platform.save();
+}
+
+export function handleRoleSet(event: RoleSet): void {
+  let user = PlatformUser.load(event.params.account.toHex());
+  if (!user) {
+    user = new PlatformUser(event.params.account.toHex());
+    user.admin = false;
+    user.contentPublisher = false;
+    user.metadataManager = false;
+  }
+
+  let admin =
+    "0x0000000000000000000000000000000000000000000000000000000000000000";
+  let contentPublisher =
+    "0xbe2fbcab6b03dc42158993aa7c9e7b92519cf9abefeafb3902d8cc76e7dd14ed";
+  let metadataManager =
+    "0x21e141d29efe528175baa3d6b347407f49288a1a3c0aebcc3160cd2b50b2a9c1";
+
+  let granted = event.params.granted;
+
+  if (event.params.role.toHex() == admin) user.admin = granted;
+  if (event.params.role.toHex() == contentPublisher)
+    user.contentPublisher = granted;
+  if (event.params.role.toHex() == metadataManager)
+    user.metadataManager = granted;
+
+  user.platform = event.params.clone.toHex();
+
+  user.save();
+}
 
 export function handleFactoryImplementationSet(
   event: FactoryImplementationSet
 ): void {}
-
-export function handlePlatformMetadataDigestSet(
-  event: PlatformMetadataDigestSet
-): void {}
-
-export function handleRoleSet(event: RoleSet): void {}
